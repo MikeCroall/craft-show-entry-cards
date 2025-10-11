fb888523382cd1d40a0994bc9bd1156568d90e78
Mike Croall <mikebcroall@gmail.com>
2025-10-11 22:02:17 +0100

    Working save link (anchor instead of button for now)

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
-use crate::render::{TypstInputs, render_to_bytes};
+use crate::render::{render_to_bytes, TypstInputs};
 
-use base64::{Engine as _, engine::general_purpose};
+use base64::{engine::general_purpose, Engine as _};
 use leptos::prelude::*;
 
 fn some_if_not_blank(signal_in: ReadSignal<String>) -> Option<String> {
@@ -13,7 +13,7 @@ pub fn App() -> impl IntoView {
     let (entrants_name, set_entrants_name) = signal("".to_string());
     let (entrants_age, set_entrants_age) = signal("".to_string());
 
-    let embed_pdf_src = Memo::new(move |_| {
+    let base64_pdf = Memo::new(move |_| {
         let contact_details = some_if_not_blank(contact_details);
         let entrants_name = some_if_not_blank(entrants_name);
         let entrants_age = some_if_not_blank(entrants_age);
@@ -22,9 +22,10 @@ pub fn App() -> impl IntoView {
             entrants_name,
             entrants_age,
         });
-        let base64_pdf = general_purpose::STANDARD.encode(&raw_pdf);
-        format!("data:application/pdf;base64,{}", base64_pdf)
+        general_purpose::STANDARD.encode(&raw_pdf)
     });
+    let embed_pdf_src =
+        Memo::new(move |_| format!("data:application/pdf;base64,{}", base64_pdf.get()));
 
     view! {
         <label>"Contact Details"</label>
@@ -36,8 +37,15 @@ pub fn App() -> impl IntoView {
         <label>"Entrant's Age"</label>
         <input type="text" bind:value=(entrants_age, set_entrants_age) />
         <br />
-        <button on:click=move |_| {}>"Save Entry Card PDF"</button>
+        <a href=embed_pdf_src download="prefilled-entry-card.pdf" target="_blank">
+            "Save Pre-filled Entry Card PDF"
+        </a>
         <br />
-        <embed src=embed_pdf_src width="100%" height="800px" />
+        <embed
+            src=embed_pdf_src
+            title="Pre-filled Entry Card Preview"
+            width="100%"
+            height="800px"
+        />
     }
 }
